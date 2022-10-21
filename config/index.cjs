fd225785ae92318eb6ccb6bb5aa28eb8f2546794
wanle07 <115018974+wanle07@users.noreply.github.com>
2022-10-21 15:43:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '52e99583658972c3042dbd26619077aa',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河南',
+  CITY: '信阳',
 
   USERS: [
     {
@@ -32,9 +32,6 @@ const USER_CONFIG = {
           type: '*生日', name: '乖乖', year: '1996', date: '07-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
         {
           type: '节日', name: '相识纪念日', year: '2021', date: '06-16',
         },
@@ -56,7 +53,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'owrLO6rIGU87uPYUG62HaeXomQmo',
     }
   ],
 
